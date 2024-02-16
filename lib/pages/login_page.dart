dc24166fae4105b94ecaf314781ad0788ab80bb5
guilherme <guilhermer_mesquita@hotmail.com>
2024-02-16 14:34:22 -0400

    final components login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -9,116 +9,148 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  int counter = 0;
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'My App',
-            style: TextStyle(color: Colors.white),
-          ),
-          backgroundColor: Colors.green,
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Ações do usuário",
-              style: GoogleFonts.montserrat(),
-            ),
-            Container(
-              color: Colors.red,
-              width: double.infinity,
-              child: Center(
-                child: Text(
-                  "foi clicado $counter vezes",
-                  style: GoogleFonts.montserrat(fontSize: 20),
+          backgroundColor: Color.fromARGB(255, 10, 12, 31),
+          body: Column(
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              Row(
+                children: [
+                  Expanded(child: Container()),
+                  Expanded(
+                    flex: 5,
+                    child: Image.network(
+                      'https://hermes.digitalinnovation.one/assets/diome/logo.png',
+                      height: 125,
+                    ),
+                  ),
+                  Expanded(child: Container()),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                'Já tem cadastro?',
+                style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                'Faça seu login e make the change._',
+                style: TextStyle(fontSize: 14, color: Colors.white),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.symmetric(horizontal: 30),
+                height: 30,
+                alignment: Alignment.center,
+                child: const Row(
+                  children: [
+                    Expanded(
+                        flex: 2,
+                        child: Text(
+                          'informe seu email:',
+                          style: TextStyle(color: Colors.white),
+                        )),
+                    Expanded(
+                      flex: 3,
+                      child: Text(
+                        'email',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )
+                  ],
                 ),
               ),
-            ),
-            Column(
-              children: [
-                const SizedBox(
-                  height: 20,
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.symmetric(horizontal: 30),
+                height: 30,
+                alignment: Alignment.center,
+                child: const Row(
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: Text(
+                        'informe sua senha:',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 3,
+                      child: Text(
+                        'senha',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )
+                  ],
                 ),
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.amber,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Container(
+                height: 30,
+                width: double.infinity,
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 30,
                 ),
-                const SizedBox(
-                  height: 20,
+                color: Colors.green,
+                alignment: Alignment.center,
+                child: Text(
+                  'Login',
+                  style: GoogleFonts.montserrat(),
                 ),
-                Container(
-                  height: 30,
-                  width: double.infinity,
-                  margin: const EdgeInsets.symmetric(
-                    horizontal: 50,
-                  ),
-                  color: Colors.green,
-                  alignment: Alignment.center,
-                  child: Text(
-                    'Informe seu email',
-                    style: GoogleFonts.montserrat(),
-                  ),
+              ),
+              Expanded(child: Container()),
+              Container(
+                height: 30,
+                width: double.infinity,
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 30,
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  height: 30,
-                  width: double.infinity,
-                  margin: const EdgeInsets.symmetric(
-                    horizontal: 50,
-                  ),
-                  color: Colors.green,
-                  alignment: Alignment.center,
-                  child: Text(
-                    'Informe sua senha',
-                    style: GoogleFonts.montserrat(),
-                  ),
-                ),
-                // SizedBox.expand(),
-                Container(
-                  height: 30,
-                  width: double.infinity,
-                  margin: const EdgeInsets.symmetric(
-                    horizontal: 80,
-                  ),
-                  color: Colors.green,
-                  alignment: Alignment.center,
-                  child: Text(
-                    'Login',
-                    style: GoogleFonts.montserrat(),
+                alignment: Alignment.center,
+                child: const Text(
+                  'Esqueci minha senha',
+                  style: TextStyle(
+                    color: Colors.yellow,
+                    fontWeight: FontWeight.w400,
                   ),
                 ),
-                // const SizedBox(
-                //   height: 30,
-                // ),
-              ],
-            )
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => {
-            setState(() {
-              counter++;
-            })
-          },
-          backgroundColor: Colors.green,
-          hoverColor: const Color.fromARGB(255, 49, 118, 51),
-          child: const Icon(
-            Icons.add_box_outlined,
-            color: Colors.white,
-          ),
-        ),
-      ),
+              ),
+              Container(
+                height: 30,
+                width: double.infinity,
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 30,
+                ),
+                alignment: Alignment.center,
+                child: const Text(
+                  'Criar Conta',
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 60,
+              )
+            ],
+          )),
     );
   }
 }
